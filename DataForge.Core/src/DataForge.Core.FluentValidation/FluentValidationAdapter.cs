447b414485d7b3c6eb24b914d110c9ccfd3d3a3a
kongliuli <kongliuli@users.noreply.github.com>
2026-05-19 02:23:00 +0000

    feat: add FluentValidation adapter for DataForge.Core validation

---
+++ DataForge.Core/src/DataForge.Core.FluentValidation/FluentValidationAdapter.cs
@@ -0,0 +1,36 @@
+using DataForge.Core.Core.Validation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataForge.Core.FluentValidation;
+
+public class FluentValidationAdapter<T> : IValidator<T>
+{
+    private readonly global::FluentValidation.IValidator<T> _innerValidator;
+
+    public FluentValidationAdapter(global::FluentValidation.IValidator<T> innerValidator)
+    {
+        _innerValidator = innerValidator;
+    }
+
+    public async Task<ValidationResult> ValidateAsync(T instance, CancellationToken cancellationToken = default)
+    {
+        var fluentResult = await _innerValidator.ValidateAsync(instance, cancellationToken);
+
+        if (fluentResult.IsValid)
+        {
+            return ValidationResult.Success();
+        }
+
+        var errors = fluentResult.Errors.Select(e => new ValidationError
+        {
+            PropertyName = e.PropertyName,
+            Message = e.ErrorMessage,
+            ErrorCode = ValidationErrorCode.Custom
+        }).ToList();
+
+        return ValidationResult.Failure(errors);
+    }
+}
